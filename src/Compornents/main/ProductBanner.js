df8a35b9d4e180e82445de13de911f1f9255d690
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-10-11 17:10:27 +0600

    added some features

--- src/Compornents/main/ProductBanner.js
+++ src/Compornents/main/ProductBanner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './pbanner.css'
 const ProductBanner = () => {
     return (
-        <div className="background-image ProductBanner">
+        <div className=" ProductBanner">
             <div className='text-8xl font-mon text-center lg:p-36 text-black'>
                 <h1 >SUPER</h1>
                 <h1 className='text-6xl'>Quality Brand</h1>
--- src/Compornents/main/submain/Submain.js
+++ src/Compornents/main/submain/Submain.js
@@ -9,10 +9,10 @@ const Submain = () => {
             <div className='lg:px-20 border'>
                 <div className='border lg:flex md:flex'>
                     <img className='w-96' src={img} alt='img' />
-                    <div className='mt-20 border outline-double'>
+                    <div className='mt-20 border outline-double lg:block md:block hidden'>
                         <h1 className='text-5xl font-sans p-2'>Super Product</h1>
                         <h1 className='text-4xl font-sans p-2'>Best SELL</h1>
-                        <Link to='/shop'><button className='btn btn-outline btn-secondary'>SEE ALL PRODUCT</button></Link>
+                        <Link to='/shop'><button className='btn btn-outline rounded-sm btn-secondary ml-2'>SEE ALL PRODUCT</button></Link>
                     </div>
                 </div>
             </div>
